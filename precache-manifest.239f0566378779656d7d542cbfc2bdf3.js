b905314f8acd57d2583d933e4173c6ee64943290
Junwon Jang <junwonjang2018@u.northwestern.edu>
2020-02-20 21:23:24 -0500

    Updates

---
+++ precache-manifest.239f0566378779656d7d542cbfc2bdf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0c9bde5c58627e6c03015fc95199be",
+    "url": "/symphony-crosswords/index.html"
+  },
+  {
+    "revision": "e889ebadec1adca422ec",
+    "url": "/symphony-crosswords/static/css/main.35e107eb.chunk.css"
+  },
+  {
+    "revision": "7b4c540aee4483c85e5e",
+    "url": "/symphony-crosswords/static/js/2.85107f9d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/symphony-crosswords/static/js/2.85107f9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e889ebadec1adca422ec",
+    "url": "/symphony-crosswords/static/js/main.2c73de87.chunk.js"
+  },
+  {
+    "revision": "15dbe29c4074f70da98c",
+    "url": "/symphony-crosswords/static/js/runtime-main.77307df3.js"
+  }
+]);
